dbdb7175a63edfb7e78a204e6941c79255e73f72
Pavel-Bylkov <oldman.bpv@gmail.com>
2021-03-02 11:51:05 +0300

    test ORM

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Blog(models.Model):
     name = models.CharField(max_length=100)
     tagline = models.TextField()
+    url = models.URLField()
 
     def __str__(self):
         return '{}'.format(self.name)
@@ -18,9 +19,10 @@ class Entry(models.Model):
     blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
     headline = models.CharField(max_length=255)
     body_text = models.TextField()
+    published = models.BooleanField(default=True)
     pub_date = models.DateField()
     mod_date = models.DateField()
-    authors = models.ManyToManyField(Author)
+    authors = models.ManyToManyField(Author, related_name="posts")
     number_of_comments = models.IntegerField()
     number_of_pingbacks = models.IntegerField()
     rating = models.IntegerField()
